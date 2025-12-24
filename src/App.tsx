6f941ff9f6e658482484924af165f1377b92b8f9
kaveh <kaveh.nassiri1998@gmail.com>
2025-12-25 01:58:05 +0330

    feat(components):add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import Form from "./component/Form";
+import "./style.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import DataTable from "./component/DataTable";
+import NotFound from "./component/notFound";
+import { Box } from "@mui/material";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#f9f6f6f0",
+          minWidth: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/form/create" />} />
+          <Route path="/form/:mode/:id?" element={<Form />} />
+          <Route path="/dataTable" element={<DataTable />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Box>
+    </>
+  );
 };
 
 export default App;
